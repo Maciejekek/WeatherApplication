ff52e1c0acb323e4024b05bb2c559610c2962b0d
Maciejekek <95121937+Maciejekek@users.noreply.github.com>
2022-04-02 09:45:53 +0200

    Location entity

---
+++ src/main/java/com/sda/weather/location/Location.java
@@ -0,0 +1,35 @@
+package com.sda.weather.location;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Location {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String city;
+    private String region;
+    private String country;
+    private float longitude;
+    private float latitude;
+
+    public Location(String city, String region, String country, float longitude, float latitude) {
+        this.city = city;
+        this.region = region;
+        this.country = country;
+        this.longitude = longitude;
+        this.latitude = latitude;
+
+    }
+
+}
