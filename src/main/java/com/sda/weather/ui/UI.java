abbbc7425e029332817f8ab2faf4985029868537
Maciejekek <95121937+Maciejekek@users.noreply.github.com>
2022-04-02 09:37:50 +0200

    ui

--- src/main/java/com/sda/weather/ui/UI.java
+++ src/main/java/com/sda/weather/ui/UI.java
@@ -3,7 +3,9 @@ package com.sda.weather.ui;
 import java.util.Scanner;
 
 public class UI {
+
     private final Scanner scanner = new Scanner(System.in);
+
     public void run(){
 
 
@@ -16,10 +18,11 @@ public class UI {
             System.out.println("3. Wyświetlić informacje o pogodzie dla lokalizacji");
             System.out.println("4. Zamknąć aplikację");
 
-            int userInput = scanner.nextInt();
+            int userInput = getInteger();
 
             switch (userInput) {
                 case 1:
+                    createLocation();
                     break;
                 case 2:
                     break;
@@ -29,6 +32,30 @@ public class UI {
                     return;
                 default:
                     System.out.println("Podana wartość musi być cyfrą. Wpisz cyfrę: ");
+            }
+        }
+    }
+
+    private void createLocation() {
+        System.out.println("Podaj nazwę miasta: ");
+        String city = scanner.nextLine();
+        System.out.println("Podaj nazwe regionu: ");
+        String region = scanner.nextLine();
+        System.out.println("Podaj nazwę kraju: ");
+        String country = scanner.nextLine();
+        System.out.println("Podaj szerokosc geograficzną: ");
+        String longitude = scanner.nextLine();
+        System.out.println("Podaj długość geograficzną: ");
+        String latitude = scanner.nextLine();
+    }
+
+    private int getInteger() {
+        while (true) {
+            try {
+                String input = scanner.nextLine();
+                return Integer.parseInt(input);
+            } catch (Exception e) {
+                System.out.println("Podana wartość musi być cyfrą! Wpisz cyfrę: ");
             }
         }
     }
